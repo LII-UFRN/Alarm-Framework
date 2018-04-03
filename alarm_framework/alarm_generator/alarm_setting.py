3950d5518c5bee84f5386587f6ac00b042d5438c
Yuri Thomas <yuri.tpinheiro@gmail.com>
2018-04-03 18:17:24 -0300

    Representation method to Alarm Setting

--- alarm_framework/alarm_generator/alarm_setting.py
+++ alarm_framework/alarm_generator/alarm_setting.py
@@ -11,3 +11,7 @@ class AlarmSetting:
         self.proc_var = proc_var
         self.on_delay = on_delay
         self.off_delay = off_delay
+
+    def __repr__(self):
+        return "Limit: %f\nType: %s\nProcess Variable: %s\n\n" \
+               % (self.limit, self.alm_type, self.proc_var)
